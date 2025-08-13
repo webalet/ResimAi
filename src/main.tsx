39a92899c0e2c4b0fbe1cec21b67066cb0d037b9
ResimAI Admin <admin@resim-ai.com>
2025-08-13 20:37:59 +0300

    Fix: Remove React.StrictMode to prevent duplicate header rendering in mobile view

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./i18n/config";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <App />
 );
